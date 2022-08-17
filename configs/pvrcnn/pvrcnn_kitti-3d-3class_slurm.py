dbca36efbb855d5d6d000e369947d599ffa8cfb9
VVsssssk <shenkun@pjlab.org.cn>
2022-08-17 19:28:21 +0800

    fix cfg

--- configs/pvrcnn/pvrcnn_kitti-3d-3class_slurm.py
+++ configs/pvrcnn/pvrcnn_kitti-3d-3class_slurm.py
@@ -17,7 +17,9 @@ db_sampler = dict(
         filter_by_difficulty=[-1],
         filter_by_min_points=dict(Car=5, Pedestrian=5, Cyclist=5)),
     classes=class_names,
-    sample_groups=dict(Car=15, Pedestrian=10, Cyclist=10))
+    sample_groups=dict(Car=15, Pedestrian=10, Cyclist=10),
+    points_loader=dict(
+        type='LoadPointsFromFile', coord_type='LIDAR', load_dim=4, use_dim=4))
 
 train_pipeline = [
     dict(type='LoadPointsFromFile', coord_type='LIDAR', load_dim=4, use_dim=4),
